386352977acab3aaaa6b753564177fd0b92f4c07
Carlos Teixeira <c.texeirajr@gmail.com>
2024-10-07 16:17:57 -0300

    instanciando objetos a partir de classes (metodos)

--- Aula02/src/aula02/Aula02.java
+++ Aula02/src/aula02/Aula02.java
@@ -1,23 +1,38 @@
 package aula02;
 
+import java.util.Scanner;
+
 public class Aula02 {
     
     public static void main(String[] args) {
         //-------------------------------------------------------------BLOCO 1
-       //instaciando objeto "c1" da classe "Caneta" 
+       //instaciando objetos
         Caneta c1 = new Caneta();
+        Scanner teclado = new Scanner(System.in);
         
-        //chamando e definindo atributos
-        c1.cor = "Azul";
+        //chamando e definindo atributos separados
         c1.ponta = 0.5f;
         c1.modelo = "Caneta Bic";
         c1.carga = 58;
+        c1.cor = "Azul";
+        c1.tampada = false;
         
+        //entrada usuário (cor da caneta)
+        System.out.print("Escola a cor da sua caneta: ");
+        String corCaneta = teclado.next();
+        c1.cor = corCaneta;
+        
+        // entrada usuário (carga da caneta)
+        System.out.print("Valor carga inicial da caneta: ");
+        int cargaCaneta = teclado.nextInt();
+        c1.carga = cargaCaneta;
+           
         //chamando métodos (procedimentos)
         c1.destampar();
-        c1.status();
         c1.rabiscar();
+        c1.status();
        
+        
         //-------------------------------------------------------------BLOCO 2
         //instaciando objeto "c2" da classe "Caneta"
         Caneta c2 = new Caneta();
--- Aula02/src/aula02/Caneta.java
+++ Aula02/src/aula02/Caneta.java
@@ -13,38 +13,15 @@ public class Caneta {
     // verificando os status do objeto criado (método que não tem retorno)
     void status(){
         System.out.println("");
-        System.out.println("------Informações--------");
+        System.out.println("---------------STATUS---------------");
         System.out.println("Modelo: " + this.modelo);
-        System.out.println("Uma caneta " + this.cor);
+        System.out.println("Cor da Caneta " + this.cor);
         System.out.println("Ponta: " + this.ponta);
         System.out.println("Carga: " + this.carga);
         System.out.println("Está tampada? " + this.tampada);
     }
     
     //definindo métodos/ações/comportamentos (procedimento não tem retorno)
-    void rabiscar(){
-        if(this.tampada == true){
-            System.out.println("ERRO! Não posso rabiscar, estou tampada");
-        }else{
-            Scanner teclado = new Scanner(System.in);
-            System.out.print("Quantos rabiscos você quer fazer? ");
-            int nRabiscos = teclado.nextInt();
-            System.out.println("Rabiscando " + nRabiscos + " vezes ... ");
-            for(int c=0; c < nRabiscos; c++){
-                this.carga -= 1;
-            }
-            if(this.carga <= 0){
-                System.out.println("O valor da carga ACABOU, compre outra caneta!");
-            } else if(this.carga > 0 && this.carga <20){
-                System.out.println("O valor da carga está acabando: " + "[" + this.carga + "]");
-                System.out.println("Valor atual da carga: " + this.carga);
-            } else{
-                System.out.println("Valor atual da carga: " + this.carga);
-            }
-            
-        }
-    }
-    
     void tampar(){
       this.tampada = true;  
     }
@@ -52,5 +29,32 @@ public class Caneta {
     void destampar(){
         this.tampada = false;
     }
+    
+    void rabiscar(){
+        if(this.tampada == true){
+            System.out.println("ERRO! Não posso rabiscar, estou tampada");
+        }else if (this.tampada == false){
+            Scanner teclado = new Scanner(System.in);
+            System.out.print("Quantos rabiscos você quer fazer com a sua caneta? ");
+            int nRabiscos = teclado.nextInt();
+            System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*");
+            System.out.println("Rabiscando " + nRabiscos + " vezes ... ");
+            System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*");
+            for(int c=0; c < nRabiscos; c++){
+                this.carga -= 1;
+                }
+            if(this.carga <= 0){
+                System.out.println("O valor da carga ACABOU, compre outra caneta!");
+            }else if(this.carga > 0 && this.carga <20){
+                System.out.println("O valor da carga está acabando: " + "[" + this.carga + "]");
+                System.out.println("Valor atual da carga: " + this.carga);
+            }else{
+                System.out.println("Valor atual da carga: " + this.carga);
+            }
+            
+        }
+    }
+    
+    
         
 }
